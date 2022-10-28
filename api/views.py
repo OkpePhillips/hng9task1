fb0bb498b3e8ba5cdab4336b2ca250e0b90681c4
godwin <okpegodwin18@yahoo.com>
2022-10-28 17:56:54 +0100

    Fixing cors

--- api/views.py
+++ api/views.py
@@ -3,5 +3,6 @@ from django.http import JsonResponse
 # Create your views here.
 
 def myView(request):
+    header = {"Access-Control-Allow-Origin":"*"}
     data = {'slackUsername': 'OkpePhillips', 'backend': True , 'age': 30 , 'bio': 'A python backend developer with passion for building useful applications'}
-    return JsonResponse(data)
+    return JsonResponse(data, headers=header)
